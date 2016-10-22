3a96ded983b49ce47d8c7f24a87e942ceee2d6f2
Ростов Сергей <snrostov@ya.ru>
2016-10-22 06:05:04 +0300

    pattern matcher apply

--- src/main/kotlin/org/srostov/simplelang/visitor/optimize/Ops.kt
+++ src/main/kotlin/org/srostov/simplelang/visitor/optimize/Ops.kt
@@ -24,23 +24,37 @@ object OptOptimizePatterns {
             Pattern(a + b, b + a),
             Pattern(a * b, b * a),
             Pattern((a + b) + c, a + (b + c)),
-            Pattern(a * (b + c), a * b + a * c)
+            Pattern(a * (b + c), a * b + a * c),
+
+            Pattern(a + (b + c), (a + b) + c),
+            Pattern(a * b + a * c, a * (b + c))
     )
 }
 
 class OpsOptimizer : Transformer<Unit>() {
     override fun visitOp(x: Operator.Call, a: Unit): Expr = with(x) {
-        val transformed = super.visitOp(x, a)
+        return tryVariant(x)
+    }
 
-        if (transformed is Operator.Call) {
+    private fun tryVariant(call: Operator.Call, depth: Int = 1): Expr {
+        if (depth > 5) return call
+
+        val t = super.visitOp(call, Unit)
+        if (t is Operator.Call) {
             OptOptimizePatterns.leafReplacements.forEach {
-                val match = it.match(transformed)
+                val match = it.match(t)
                 if (match != null) {
                     return match.transform(it.cause)
                 }
             }
-        }
 
-        return transformed
+            OptOptimizePatterns.variantsReplacements.forEach {
+                val match = it.match(t)
+                if (match != null) {
+                    return tryVariant(match.transform(it.cause) as Operator.Call, depth + 1)
+                }
+            }
+        }
+        return t
     }
 }
--- src/test/kotlin/org/srostov/simplelang/visitor/optimize/OpsOptimizerTest.kt
+++ src/test/kotlin/org/srostov/simplelang/visitor/optimize/OpsOptimizerTest.kt
@@ -7,12 +7,17 @@ import org.srostov.simplelang.visitor.toStr
 
 class OpsOptimizerTest : BaseTest() {
     val x = UnknownExpr("x")
+    val y = UnknownExpr("y")
 
     @Test
     fun test1() = Assert.assertEquals("x + x", optimize(x + 0.asConst + x + 0.asConst))
 
     @Test
     fun test2() = Assert.assertEquals("x", optimize(x * 0.asConst + x + 0.asConst))
+
+
+    @Test
+    fun test3() = Assert.assertEquals("y * x", optimize(x * (0.asConst + y)))
 
     private fun optimize(expr: Expr) = expr.accept(OpsOptimizer(), Unit).toStr()
 }
